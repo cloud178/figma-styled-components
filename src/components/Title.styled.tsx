510590e5deb87c5b667e3f96fde4054cc24173ed
Denis Biryukov <denis-biryukov-13@mail.ru>
2024-11-06 18:35:25 +0300

    replace div to h1 in TitleStyled component

--- src/components/Title.styled.tsx
+++ src/components/Title.styled.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 
-export const TitleStyled = styled.div`
+export const TitleStyled = styled.h1`
     font-weight: 700;
     font-size: 16px;
     line-height: 19.36px;
